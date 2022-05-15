1510b96274ff96b2e04c37a9584e52f1bbc4c6dc
GoldOstap <izzheurov@mail.ru>
2022-05-16 00:20:02 +0400

    переписал циклы

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int k = 0;
+
         int min=0; // минимальное значение
         int max=10; // максимальное значение
-        Random r = new Random();
+        Random rnd = new Random();
         Scanner scan=new Scanner(System.in);
         System.out.print("Введите размер массива: ");
-        k = scan.nextInt();
+        int k = scan.nextInt();
         int[] myArray = new int[k];
-        for (int t=0;t<myArray.length; t++) {
-            myArray[t] = r.nextInt((max - min) + 1) + min;
-        }
-        for (int t=0;t<myArray.length; t++) {
-            System.out.print(myArray[t]+" ");
-        }
+
+        for (int t=0;t<myArray.length; t++)
+            myArray[t] = rnd.nextInt((max - min) + 1) + min;
+
+        for (int i : myArray)
+            System.out.print(i + " ");
+
         System.out.println("\nВсе четные");
-        for (int t=0; t< myArray.length; t++){
-            if (myArray[t]%2 ==0 & myArray[t] !=0) System.out.print(myArray[t]+" ");
+        for (int i : myArray) {
+            if (i % 2 == 0 & i != 0) System.out.print(i + " ");
         }
     }
 }
